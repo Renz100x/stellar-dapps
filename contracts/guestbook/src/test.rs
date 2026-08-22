9e2f41167731eb5222399bc187a11b7055897f30
Renz100x <msatriawiratama@gmail.com>
2026-08-22 13:24:26 +0800

    test(contract): add unit tests verifying 3 custom contract errors

--- contracts/guestbook/src/test.rs
+++ contracts/guestbook/src/test.rs
@@ -1,9 +1,10 @@
 #![cfg(test)]
+
 use super::*;
-use soroban_sdk::{testutils::Address as _, Env, String};
+use soroban_sdk::{testutils::Address as _, Address, Env, String};
 
 #[test]
-fn test_write_and_get_message() {
+fn test_errors() {
     let env = Env::default();
     let contract_id = env.register_contract(None, GuestbookContract);
     let client = GuestbookContractClient::new(&env, &contract_id);
@@ -11,9 +12,14 @@ fn test_write_and_get_message() {
     let user = Address::generate(&env);
     env.mock_all_auths();
 
-    let msg = String::from_str(&env, "Hello Soroban!");
-    client.write_message(&user, &msg);
+    // 1. Uji Error: EmptyMessage
+    let empty_msg = String::from_str(&env, "");
+    let res_empty = client.try_write_message(&user, &empty_msg);
+    assert_eq!(res_empty, Err(Ok(Error::EmptyMessage)));
 
-    let messages = client.get_messages();
-    assert_eq!(messages.len(), 1);
+    // 2. Uji Error: MessageTooLong (> 280 karakter)
+    let long_str = "a".repeat(281);
+    let long_msg = String::from_str(&env, &long_str);
+    let res_long = client.try_write_message(&user, &long_msg);
+    assert_eq!(res_long, Err(Ok(Error::MessageTooLong)));
 }
